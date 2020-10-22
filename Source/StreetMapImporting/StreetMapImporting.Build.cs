3d4bed4878f7ef12c0139f6ad3c3c2180fb59379
Iwer Petersen <iwer.petersen@haw-hamburg.de>
2020-10-22 21:01:26 +0200

    fix packaging on 4.25
    using mhousse1247's fix from https://github.com/ue4plugins/StreetMap/pull/46

--- Source/StreetMapImporting/StreetMapImporting.Build.cs
+++ Source/StreetMapImporting/StreetMapImporting.Build.cs
@@ -1,35 +1,37 @@
 // Copyright 2017 Mike Fricker. All Rights Reserved.
 
-namespace UnrealBuildTool.Rules
-{
-  public class StreetMapImporting : ModuleRules
-  {
-    public StreetMapImporting(ReadOnlyTargetRules Target)
-    : base(Target)
-    {
-      PrivateDependencyModuleNames.AddRange(
-        new string[] {
-          "Core",
-          "CoreUObject",
-          "Engine",
-          "UnrealEd",
-          "XmlParser",
-          "AssetTools",
-          "Projects",
-          "Slate",
-          "EditorStyle",
-          "SlateCore",
-          "PropertyEditor",
-          "RenderCore",
-          "RHI",
-          "RawMesh",
-          "AssetTools",
-          "AssetRegistry",
-          "StreetMapRuntime"
-        }
-      );
+using UnrealBuildTool;
 
-      PrivateIncludePaths.AddRange(new string[]{"StreetMapImporting/Private"});
+public class StreetMapImporting : ModuleRules
+{
+    public StreetMapImporting(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PrivateIncludePaths.AddRange(
+            new string[]{
+                "StreetMapImporting/Private"
+            }
+        );
+        PrivateDependencyModuleNames.AddRange(
+            new string[] {
+              "Core",
+              "CoreUObject",
+              "Engine",
+              "UnrealEd",
+              "XmlParser",
+              "AssetTools",
+              "Projects",
+              "Slate",
+              "EditorStyle",
+              "SlateCore",
+              "PropertyEditor",
+              "RenderCore",
+              "RHI",
+              "RawMesh",
+              "AssetTools",
+              "AssetRegistry",
+              "StreetMapRuntime"
+            }
+        );
+
     }
-  }
 }
--- Source/StreetMapRuntime/StreetMapRuntime.Build.cs
+++ Source/StreetMapRuntime/StreetMapRuntime.Build.cs
@@ -1,24 +1,28 @@
 // Copyright 2017 Mike Fricker. All Rights Reserved.
 
-namespace UnrealBuildTool.Rules
-{
-  public class StreetMapRuntime : ModuleRules
-  {
-    public StreetMapRuntime(ReadOnlyTargetRules Target)
-    : base(Target)
-    {
-      PrivateDependencyModuleNames.AddRange(
-        new string[] {
-          "Core",
-          "CoreUObject",
-          "Engine",
-          "RHI",
-          "RenderCore",
-          "PropertyEditor"
-        }
-      );
+using UnrealBuildTool;
 
-      PrivateIncludePaths.AddRange(new string[]{"StreetMapRuntime/Private"});
+public class StreetMapRuntime : ModuleRules
+{
+    public StreetMapRuntime(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PrivateDependencyModuleNames.AddRange(
+            new string[] {
+                "Core",
+                "CoreUObject",
+                "Engine",
+                "RHI",
+                "RenderCore",
+            }
+        );
+
+        PrivateIncludePaths.AddRange(new string[]{"StreetMapRuntime/Private"});
+
+        if (Target.bBuildEditor == true)
+        {
+            //@TODO: Needed for FPropertyEditorModule::NotifyCustomizationModuleChanged()
+            //@TODO: To move/implement in FStreetMapComponentDetails
+            PrivateDependencyModuleNames.Add("PropertyEditor");
+        }
     }
-  }
 }
